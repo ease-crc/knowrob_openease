0a7e911ff67bd50f4b0c3759ff859c0f4389f5aa
Sascha Jongebloed <sasjonge@uni-bremen.de>
2020-09-15 17:07:53 +0200

    started marker vis subpkg

--- src/marker_vis/vis.pl
+++ src/marker_vis/vis.pl
@@ -3,10 +3,14 @@
       hide(r)
     ]).
 
-show(Object) :-
-	true.
+show(_) :-
+	tripledb_load('package://knowrob/owl/robots/PR2.owl'), 
+	urdf_load('http://knowrob.org/kb/PR2.owl#PR2_0', 'package://knowrob/urdf/pr2_for_unit_tests.urdf', [load_rdf]), 
+	urdf_set_pose_to_origin('http://knowrob.org/kb/PR2.owl#PR2_0',map), 
+	sleep(1.0), 
+	marker_plugin:republish.
 
-hide(Object) :-
+hide(_) :-
 	true.
 
 %:- module(knowrob_vis,
