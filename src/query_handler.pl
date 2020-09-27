d9b26a3c55204639c72b35fa605dba5549632b52
Sascha Jongebloed <sasjonge@uni-bremen.de>
2020-09-27 23:15:04 +0200

    Added first query handling

--- src/query_handler.pl
+++ src/query_handler.pl
@@ -1,6 +1,10 @@
 :- module(query_handler,
-    [ openease_query(t,r)
+    [ 
+    	openease_query(t,r),
+    	gen_msgs(t)
     ]).
+
+%:- use_module(library(data_vis)).
 
 /** <module> Tunnels queries of openEASE and executes other actions if necessary.
 
@@ -8,7 +12,7 @@
 @license BSD
 */
 
-%% openease_query(Query, Mode) is nondet.
+%% openease_query(+Query, +Mode) is nondet.
 %
 % True for statements that hold during the whole
 % duration of some time interval.
@@ -17,4 +21,20 @@
 % @param Mode A list of modes for the execution.
 %
 openease_query(Query, Mode) :-
-      call(Query).
+    call(Query),
+    gen_msgs(Query).
+
+gen_msgs(is_event(_)) :-
+ 	setof(E,
+ 		(
+ 			is_event(E),
+ 			not(is_action(E))
+ 		),Events),
+ 	data_vis:timeline(Events),
+ 	%%
+    member(NextEvent,Events),
+    create_marker(NextEvent).
+
+create_marker(Event) :-
+	interval(Event,[T1,_]),
+	show_markers(T1).
