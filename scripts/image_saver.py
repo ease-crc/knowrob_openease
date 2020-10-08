c58be276137f9dcb98d1bc41787accf71ba861cb
Daniel Beßler <danielb@uni-bremen.de>
2020-10-08 20:28:53 +0200

    added image_saver to launch file

--- scripts/image_saver.py
+++ scripts/image_saver.py
@@ -29,11 +29,10 @@ def image_callback(msg):
 	try:
 		# Convert your ROS Image message to OpenCV2
 		cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
-	except CvBridgeError, e:
-		print(e)
-	else:
 		# Save your OpenCV2 image as a jpeg 
 		cv2.imwrite(filepath, cv2_img)
+	except e:
+		print(e)
 
 if __name__ == '__main__':
 	rospy.init_node('image_listener')
