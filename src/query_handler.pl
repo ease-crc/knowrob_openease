6a63343d90b6eb3a1de092de4480e15d3432bbc1
Daniel Beßler <danielb@uni-bremen.de>
2020-10-09 20:27:59 +0200

    timeline

--- src/query_handler.pl
+++ src/query_handler.pl
@@ -36,8 +36,8 @@ get_event_data(E,Task,Start,End) :-
 
 gen_msgs(is_event(_)) :-
     findall([E,Task,Start,End],(get_event_data(E,Task,Start,End)),EventData),
-    %data_vis:timeline_data(EventData),
-     %%
+    data_vis:timeline_data(EventData),
+    %%
     findall(T0,member([_,_,T0,_],EventData),T0s),
     member(Time,T0s),
     marker_plugin:show_markers(Time).
