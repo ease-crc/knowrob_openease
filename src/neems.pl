2071808f022f958e8dd311a12f284717d7096801
Daniel Beßler <danielb@uni-bremen.de>
2020-11-15 19:53:30 +0100

    cleanup

--- src/neems.pl
+++ src/neems.pl
@@ -7,11 +7,8 @@ neem_init(NEEM_id) :-
 	set_setting(mng_client:collection_prefix, NEEM_id),
 	% load URDF files referred to in triple store
 	urdf_init,
-writeln(load_init_tf),
 	% initialize position of each frame for tf publishing
 	tf_tree:initial_transforms(InitialTransforms),
-length(InitialTransforms,NInitialTransforms),
-writeln(initial_transforms(NInitialTransforms)),
 	forall(
 		member([Ref,Frame,Pos,Rot],InitialTransforms),
 		tf_plugin:tf_republish_set_pose(Frame,[Ref,Pos,Rot])
